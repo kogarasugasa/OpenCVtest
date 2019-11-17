eb2e3582297833b97415a5fdb0ceb694e22e6bfc
隼斗 小林 <wahuku.miyatuguti_35@hotmail.co.jp>
2019-11-17 15:10:50 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenCVtest/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace OpenCVtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StdButton1 = new System.Windows.Forms.Button();
+            this.GrayButton1 = new System.Windows.Forms.Button();
+            this.MatchButton1 = new System.Windows.Forms.Button();
+            this.BorderLineTextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MatchButton2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.PictureTextBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PictureTextBox2 = new System.Windows.Forms.TextBox();
+            this.CaptureButton1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StdButton1
+            // 
+            this.StdButton1.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.StdButton1.Location = new System.Drawing.Point(40, 30);
+            this.StdButton1.Name = "StdButton1";
+            this.StdButton1.Size = new System.Drawing.Size(200, 75);
+            this.StdButton1.TabIndex = 0;
+            this.StdButton1.Text = "基本";
+            this.StdButton1.UseVisualStyleBackColor = true;
+            this.StdButton1.Click += new System.EventHandler(this.StdButton1_Click);
+            // 
+            // GrayButton1
+            // 
+            this.GrayButton1.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.GrayButton1.Location = new System.Drawing.Point(40, 111);
+            this.GrayButton1.Name = "GrayButton1";
+            this.GrayButton1.Size = new System.Drawing.Size(200, 75);
+            this.GrayButton1.TabIndex = 1;
+            this.GrayButton1.Text = "グレースケール";
+            this.GrayButton1.UseVisualStyleBackColor = true;
+            this.GrayButton1.Click += new System.EventHandler(this.GrayButton1_Click);
+            // 
+            // MatchButton1
+            // 
+            this.MatchButton1.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MatchButton1.Location = new System.Drawing.Point(40, 192);
+            this.MatchButton1.Name = "MatchButton1";
+            this.MatchButton1.Size = new System.Drawing.Size(200, 75);
+            this.MatchButton1.TabIndex = 2;
+            this.MatchButton1.Text = "ﾃﾝﾌﾟﾚｰﾄﾏｯﾁﾝｸﾞ";
+            this.MatchButton1.UseVisualStyleBackColor = true;
+            this.MatchButton1.Click += new System.EventHandler(this.MatchButton1_Click);
+            // 
+            // BorderLineTextBox1
+            // 
+            this.BorderLineTextBox1.Location = new System.Drawing.Point(305, 108);
+            this.BorderLineTextBox1.Name = "BorderLineTextBox1";
+            this.BorderLineTextBox1.Size = new System.Drawing.Size(50, 19);
+            this.BorderLineTextBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "閾値";
+            // 
+            // MatchButton2
+            // 
+            this.MatchButton2.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MatchButton2.Location = new System.Drawing.Point(40, 273);
+            this.MatchButton2.Name = "MatchButton2";
+            this.MatchButton2.Size = new System.Drawing.Size(200, 75);
+            this.MatchButton2.TabIndex = 5;
+            this.MatchButton2.Text = "ﾃﾝﾌﾟﾚｰﾄﾏｯﾁﾝｸﾞ2";
+            this.MatchButton2.UseVisualStyleBackColor = true;
+            this.MatchButton2.Click += new System.EventHandler(this.MatchButton2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(246, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "検索範囲";
+            // 
+            // PictureTextBox1
+            // 
+            this.PictureTextBox1.Location = new System.Drawing.Point(305, 27);
+            this.PictureTextBox1.Name = "PictureTextBox1";
+            this.PictureTextBox1.Size = new System.Drawing.Size(300, 19);
+            this.PictureTextBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(246, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "検索画像";
+            // 
+            // PictureTextBox2
+            // 
+            this.PictureTextBox2.Location = new System.Drawing.Point(305, 64);
+            this.PictureTextBox2.Name = "PictureTextBox2";
+            this.PictureTextBox2.Size = new System.Drawing.Size(300, 19);
+            this.PictureTextBox2.TabIndex = 9;
+            // 
+            // CaptureButton1
+            // 
+            this.CaptureButton1.Font = new System.Drawing.Font("Meiryo UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.CaptureButton1.Location = new System.Drawing.Point(40, 354);
+            this.CaptureButton1.Name = "CaptureButton1";
+            this.CaptureButton1.Size = new System.Drawing.Size(200, 75);
+            this.CaptureButton1.TabIndex = 10;
+            this.CaptureButton1.Text = "キャプチャ";
+            this.CaptureButton1.UseVisualStyleBackColor = true;
+            this.CaptureButton1.Click += new System.EventHandler(this.CaptureButton1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(255, 136);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(717, 413);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CaptureButton1);
+            this.Controls.Add(this.PictureTextBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PictureTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MatchButton2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BorderLineTextBox1);
+            this.Controls.Add(this.MatchButton1);
+            this.Controls.Add(this.GrayButton1);
+            this.Controls.Add(this.StdButton1);
+            this.Name = "Form1";
+            this.Text = "sample_show";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StdButton;
+        private System.Windows.Forms.Button StdButton1;
+        private System.Windows.Forms.Button GrayButton1;
+        private System.Windows.Forms.Button MatchButton1;
+        private System.Windows.Forms.TextBox BorderLineTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MatchButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox PictureTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PictureTextBox2;
+        private System.Windows.Forms.Button CaptureButton1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ OpenCVtest/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+
+namespace OpenCVtest
+{
+    public partial class Form1 : Form
+    {
+        private string picturePath1;
+        private string picturePath2;
+        private bool isPreview = false;
+        private Timer captureTimer = new Timer();
+        VideoCapture camera = null;
+        private Mat frame = null;
+        Graphics graphic;
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            BorderLineTextBox1.Text = "0.9";
+            PictureTextBox1.Text = @"E:\作業\A001.jpg";
+            PictureTextBox2.Text = @"E:\作業\A002.jpg";
+
+            //captureTimer.Tick += new EventHandler(this.Capture);
+            //captureTimer.Interval = 200;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CaptureStop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StdButton1_Click(object sender, EventArgs e)
+        {
+            GetPicturePath(ref picturePath1,ref picturePath2);
+
+            using (Mat mat = new Mat(picturePath1))
+            {
+                Cv2.ImShow("sample_show", mat);
+            }
+
+        }
+
+        private void GrayButton1_Click(object sender, EventArgs e)
+        {
+            using (Mat mat = new Mat(@"E:\作業\A001.jpg"))
+            using (Mat matGray = mat.CvtColor(ColorConversionCodes.BGR2GRAY))
+            {
+                Cv2.ImShow("grayscale_show", matGray);
+
+                //画像を保存
+                Cv2.ImWrite(@"E:\作業\B001.jpg", mat);
+
+                //画像の切り欠き保存
+                var mat2 = mat.Clone(new Rect(100, 100, 200, 150));
+                Cv2.ImWrite(@"E:\作業\C001.jpg", mat2);
+            }
+        }
+
+        private void MatchButton1_Click(object sender, EventArgs e)
+        {
+            GetPicturePath(ref picturePath1, ref picturePath2);
+
+            //検索対象の画像とテンプレートが画像
+            using (Mat mat = new Mat(picturePath1))
+            using (Mat temp = new Mat(picturePath2))
+            using (Mat result = new Mat())
+            {
+                //テンプレートマッチ
+                Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
+
+                //類似度が最大/最小となる画素の位置を調べる
+                OpenCvSharp.Point minloc, maxloc;
+                double minval, maxval;
+                Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                //閾値で判断
+                double threshold;
+                double.TryParse(BorderLineTextBox1.Text, out threshold);
+                if (threshold <= 0)
+                {
+                    threshold = 0.9;
+                }
+
+                if (maxval >= threshold)
+                {
+                    //最も見つかった場所に赤枠を表示
+                    Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
+                    Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
+
+                    //ウィンドウに画像を表示
+                    Cv2.ImShow("template1_show", mat);
+                }
+                else
+                {
+                    //見つからない
+                    MessageBox.Show("見つかりませんでした");
+                }
+
+            }
+        }
+
+        private void MatchButton2_Click(object sender, EventArgs e)
+        {
+            GetPicturePath(ref picturePath1, ref picturePath2);
+
+            // 検索対象の画像とテンプレート画像
+            using (Mat mat = new Mat(picturePath1))
+            using (Mat temp = new Mat(picturePath2))
+            using (Mat result = new Mat())
+            {
+
+                // テンプレートマッチ
+                Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
+
+                // しきい値の範囲に絞る
+                Cv2.Threshold(result, result, 0.8, 1.0, ThresholdTypes.Tozero);
+
+                while (true)
+                {
+                    // 類似度が最大/最小となる画素の位置を調べる
+                    OpenCvSharp.Point minloc, maxloc;
+                    double minval, maxval;
+                    Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                    var threshold = 0.8;
+                    if (maxval >= threshold)
+                    {
+
+                        // 見つかった場所に赤枠を表示
+                        Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
+                        Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
+
+                        // 見つかった箇所は塗りつぶす
+                        Rect outRect;
+                        Cv2.FloodFill(result, maxloc, new OpenCvSharp.Scalar(0), out outRect, new OpenCvSharp.Scalar(0.1),
+                                    new OpenCvSharp.Scalar(1.0), FloodFillFlags.Link4);
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                // ウィンドウに画像を表示
+                Cv2.ImShow("template2_show", mat);
+            }
+        }
+
+        private void CaptureButton1_Click(object sender, EventArgs e)
+        {
+            
+            if (isPreview)
+            {
+                //captureTimer.Stop();
+                CaptureStop();
+                isPreview = false;
+            }
+            else
+            {
+                //captureTimer.Start();
+                CaptureStart();
+                isPreview = true;
+            }
+        }
+
+        public void CaptureStart()
+        {
+            //カメラ画像取得用のVideoCapture作成
+            camera = new VideoCapture(0);
+            if (!camera.IsOpened())
+            {
+                MessageBox.Show("cannot open camera");
+                this.Close();
+            }
+            int width = 702;
+            int height = 405;
+            camera.FrameWidth = width;
+            camera.FrameHeight = height;
+
+            //取得先のMat作成
+            frame = new Mat(height, width, MatType.CV_8UC3);
+
+            ////表示用のBitmap作成
+            //bmp = new Bitmap(frame.Cols, frame.Rows, (int)frame.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, frame.Data);
+
+            //PictureBoxを出力サイズに合わせる
+            pictureBox1.Width = frame.Cols;
+            pictureBox1.Height = frame.Rows;
+
+            //描画用のGraphics作成
+            graphic = pictureBox1.CreateGraphics();
+
+            //画像取得スレッド開始
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private void CaptureStop()
+        {
+            Cv2.ImWrite(@"Z001.jpg", frame);
+            //スレッドの終了を待機
+            backgroundWorker1.CancelAsync();
+            while (backgroundWorker1.IsBusy)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = (BackgroundWorker)sender;
+
+            while (!backgroundWorker1.CancellationPending)
+            {
+                //画像取得
+                camera.Read(frame);
+                bw.ReportProgress(0);
+            }
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //描画
+            //graphic.DrawImage(bmp, 0, 0, frame.Cols, frame.Rows);
+            TemprateMatch(frame);
+            Image dispImage;
+            dispImage = BitmapConverter.ToBitmap(frame);
+            pictureBox1.Image = dispImage;
+
+
+        }
+        private void GetPicturePath(ref string filePath1, ref string filePath2)
+        {
+            filePath1 = PictureTextBox1.Text;
+            filePath2 = PictureTextBox2.Text;
+        }
+
+        private bool TemprateMatch(Mat mat)
+        {
+            GetPicturePath(ref picturePath1, ref picturePath2);
+
+            //検索対象の画像とテンプレートが画像
+            using (Mat temp = new Mat(picturePath2))
+            using (Mat result = new Mat())
+            {
+                //テンプレートマッチ
+                Cv2.MatchTemplate(mat, temp, result, TemplateMatchModes.CCoeffNormed);
+
+                //類似度が最大/最小となる画素の位置を調べる
+                OpenCvSharp.Point minloc, maxloc;
+                double minval, maxval;
+                Cv2.MinMaxLoc(result, out minval, out maxval, out minloc, out maxloc);
+
+                //閾値で判断
+                double threshold;
+                double.TryParse(BorderLineTextBox1.Text, out threshold);
+                if (threshold <= 0)
+                {
+                    threshold = 0.9;
+                }
+
+                if (maxval >= threshold)
+                {
+                    //最も見つかった場所に赤枠を表示
+                    Rect rect = new Rect(maxloc.X, maxloc.Y, temp.Width, temp.Height);
+                    Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+        
+    }
+}
